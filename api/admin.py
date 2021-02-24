9b4a4e32d2f52b373d1dbab7e5a27019d1f22176
ahmed atta <ahmedatta3322@gmail.com>
2021-02-25 00:35:18 +0200

    deploy

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Customers ,Recipes,Ingredients,List
 # Register your models here.
+
+Models = [Customers, Recipes, Ingredients, List]
+
+admin.site.register(Models)
--- api/models.py
+++ api/models.py
@@ -7,7 +7,20 @@ class Customers(models.Model):
     last_name = models.CharField(max_length = 30)
     email = models.CharField(max_length = 30)
     password = models.CharField(max_length = 30)
+    
+
+class Ingredients(models.Model):
+    title = models.CharField(max_length=30)
+    images = ArrayField(models.CharField(max_length=30))
+    price = models.IntegerField()
+    
+
 class Recipes(models.Model):
     title = models.CharField(max_length=30)
     perpare = models.CharField(max_length=30)
     images = ArrayField(models.CharField(max_length=30))
+    ingredients = models.ManyToManyField(Ingredients)
+class List(models.Model):
+    ingredients = models.ManyToManyField(Ingredients)
+    recipes = models.ManyToManyField(Recipes)
+    customers = models.ForeignKey(Customers,on_delete=models.CASCADE)
